1b7e6f7e7a49a8e65a3f610fc188c9a2001c32cf
alyoshas <evelynschmitz1@gmail.com>
2018-04-24 18:48:24 -0300

    FEA : Add configurações do Vuelidate.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App';
 import router from './router';
 import EventBus from './plugins/event-bus';
@@ -8,6 +9,7 @@ import EventBus from './plugins/event-bus';
 Vue.config.productionTip = false;
 
 Vue.use(EventBus);
+Vue.use(Vuelidate);
 
 
 /* eslint-disable no-new */
