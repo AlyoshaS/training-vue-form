198b91e49868fe0027ddf6dce45e97c012afd6fb
alyoshas <evelynschmitz1@gmail.com>
2018-04-25 06:46:29 -0300

    ENENH : Implementação do formulário de sign-up.

--- src/features/auth/sign-up.vue
+++ src/features/auth/sign-up.vue
@@ -1,35 +1,147 @@
 <template>
   <div id="SignUp">
-    <div class="sign-up-htm">
+    <form action="#" @submit.prevent="submit" class="sign-up-htm">
       <div class="group">
-        <label for="sign-up-user" class="label">Username</label>
-        <input id="sign-up-user" type="text" class="input">
+
+        <label
+          :class="{ invalid: $v.username.$dirty && $v.username.$invalid }"
+          for="sign-up-user"
+          class="label">Username</label>
+
+        <input
+          :class="{ invalid: $v.username.$dirty && $v.username.$invalid }"
+          @input="$v.username.$touch()"
+          id="sign-up-user"
+          type="text"
+          class="input"
+          v-model="username"
+        >
+
       </div>
       <div class="group">
-        <label for="sign-up-pass1" class="label">Password</label>
-        <input id="sign-up-pass1" type="password" class="input" data-type="password">
+
+        <label
+          :class="{ invalid: $v.password.$dirty && $v.password.$invalid }"
+          for="sign-up-pass1"
+          class="label">Password</label
+        >
+
+        <input
+          :class="{ invalid: $v.password.$dirty && $v.password.$invalid }"
+          @input="$v.password.$touch()"
+          id="sign-up-pass1"
+          type="password"
+          class="input"
+          data-type="password"
+          v-model="password"
+        >
+
       </div>
       <div class="group">
-        <label for="sign-up-pass2" class="label">Repeat Password</label>
-        <input id="sign-up-pass2" type="password" class="input" data-type="password">
+
+        <label
+          :class="{ invalid: $v.password2.$dirty && $v.password2.$invalid }"
+          for="sign-up-pass2"
+          class="label">Repeat Password</label>
+
+        <input
+          :class="{ invalid: $v.password2.$dirty && $v.password2.$invalid }"
+          @input="$v.password2.$touch()"
+          id="sign-up-pass2"
+          type="password"
+          class="input"
+          data-type="password"
+          v-model="password2"
+        >
+
       </div>
       <div class="group">
-        <label for="pass" class="label">Email Address</label>
-        <input id="pass" type="text" class="input">
+
+        <label
+          :class="{ invalid: $v.email.$dirty && $v.email.$invalid }"
+          for="email"
+          class="label">Email Address</label>
+
+        <input
+          :class="{ invalid: $v.email.$dirty && $v.email.$invalid }"
+          @input="$v.email.$touch()"
+          id="email"
+          type="text"
+          class="input"
+          v-model="email"
+        >
+
       </div>
       <div class="group">
-        <input type="submit" class="button" value="Sign Up">
+
+        <input
+          type="submit"
+          class="button"
+          value="Sign Up"
+        >
+
       </div>
       <div class="hr"></div>
+
       <div class="foot-lnk">
+
         <label for="tab-1">Already Member?</label>
-      </div>
-    </div>
+
+        </div>
+    </form>
   </div>
 </template>
 
 <script>
+import { required, sameAs, email } from 'vuelidate/lib/validators';
+
 export default {
   name: 'SignUp',
+  mounted() {
+    this.$bus.$on('navigate', this.reset);
+  },
+  validations: {
+    username: {
+      required,
+    },
+    password: {
+      required,
+    },
+    password2: {
+      required,
+      sameAsPassword: sameAs('password'),
+    },
+    email: {
+      required,
+      email,
+    },
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+      password2: '',
+      email: '',
+    };
+  },
+  methods: {
+    submit() {
+      if (!this.$v.$invalid) {
+        this.$emit('do-sign-up', { ...this.$data });
+      } else {
+        this.$v.$touch();
+      }
+    },
+    reset(selected) {
+      if (selected === 'signup') {
+        this.username = '';
+        this.password = '';
+        this.password2 = '';
+        this.email = '';
+        this.$v.$reset();
+      }
+    },
+  },
 };
 </script>
+
